ba374d68eb34439850db5463cc1dcab6c88bb71d
van <razl92@163.com>
2020-05-16 15:31:28 +0800

    添加配置文件

--- gateway/src/main/java/com/lzw/gateway/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/lzw/gateway/gateway/GatewayApplication.java
@@ -3,13 +3,13 @@ package com.lzw.gateway.gateway;
 import com.lzw.config.BasicProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.core.env.Environment;
 
 import java.net.UnknownHostException;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayApplication {
 
 	public static void main(String[] args) throws UnknownHostException {
