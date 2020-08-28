b9035745cc071f67f0e4a3ce933126135083f94b
awkwordMao <2624824150@qq.com>
2020-08-28 10:29:45 +0800

    security-jwt

---
+++ security-jwt/src/main/java/com/lj/securityjwt/SecurityJwtApplication.java
@@ -0,0 +1,17 @@
+package com.lj.securityjwt;
+
+import org.springframework.beans.factory.support.GenericBeanDefinition;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityJwtApplication.class, args);
+	}
+	public void init(){
+		GenericBeanDefinition
+	}
+
+}
